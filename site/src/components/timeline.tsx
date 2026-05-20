05e03872f553dc0fe871206043e7629de7382b8d
kg55kmr <kg55kmr@ukr.net>
2026-05-20 10:18:37 +0300

    update

--- site/src/components/timeline.tsx
+++ site/src/components/timeline.tsx
@@ -23,13 +23,14 @@ type ComponentsProps<T extends object> = {
 export function Timeline<T extends object>(
   props: Props<T> & ComponentsProps<T>,
 ) {
+  const items = props.items.toReversed();
   return (
     <Tabs
       orientation="vertical"
-      defaultValue={props.items[0].date}
+      defaultValue={items[0].date}
       listClassName="grid grid-cols-[repeat(var(--cols-count),auto)] self-start"
     >
-      {props.items.map((item) => (
+      {items.map((item) => (
         <Tabs.Tab
           key={item.date}
           title={<props.titleComponent date={item.date} />}
@@ -78,9 +79,11 @@ function MeetingsItems(props: { items: TimelineItem<Responsible>[] }) {
   return (
     <ol className="list-counter ml-8">
       {props.items.map((v) => (
-        <li key={v.text}>
+        <li key={v.text} className="mb-5">
           {v.text}
-          <div className="font-bold">{v.responsible}</div>
+          <div className="text-right font-bold sm:text-left">
+            {v.responsible}
+          </div>
           {v.items && <MeetingsItems items={v.items} />}
         </li>
       ))}

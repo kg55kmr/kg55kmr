954bb51b25c90b5708b65f059ec3c54635e3a42c
Alexander <asv7c2@gmail.com>
2026-05-29 21:13:24 +0300

    Update dev.ts

--- packages/posts/src/dev.ts
+++ packages/posts/src/dev.ts
@@ -2,13 +2,10 @@ import { write } from "./local";
 import { processPosts } from "./process";
 import { getRoot } from "./root";
 
-const root = await getRoot();
-
 export async function getPosts() {
+  const root = await getRoot();
   const data = await processPosts(root);
   write(root, data);
 
   return data;
 }
-
-await getPosts();

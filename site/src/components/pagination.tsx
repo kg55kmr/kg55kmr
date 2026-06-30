11b7db25523642a7db35521aa109020ee9bf3e1a
kg55kmr <kg55kmr@ukr.net>
2026-06-30 14:54:20 +0300

    update

--- site/src/components/pagination.tsx
+++ site/src/components/pagination.tsx
@@ -100,7 +100,7 @@ export function Pagination(props: PaginationProps) {
 }
 
 function validateInput(node: HTMLInputElement, totalPages: number) {
-  let inputPage = Number(node.value, 10);
+  let inputPage = Number(node.value);
   switch (true) {
     case Number.isNaN(inputPage):
       node.blur();
